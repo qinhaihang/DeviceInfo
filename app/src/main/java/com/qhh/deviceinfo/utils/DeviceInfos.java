e448e636e6248f76e725aab5510d66b1101fe592
qinhaihang_vendor <qinhaihang_vendor@sensetime.com>
2019-09-10 16:44:22 +0800

    1、增加dpi系统适配

--- app/src/main/java/com/qhh/deviceinfo/utils/DeviceInfos.java
+++ app/src/main/java/com/qhh/deviceinfo/utils/DeviceInfos.java
@@ -1,7 +1,10 @@
 package com.qhh.deviceinfo.utils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 import com.qhh.deviceinfo.bean.ScreenInfos;
 
@@ -21,7 +24,8 @@ public class DeviceInfos {
 
         ScreenInfos screenInfos = new ScreenInfos();
 
-        DisplayMetrics metrics = new DisplayMetrics();
+        //DisplayMetrics metrics = new DisplayMetrics();
+        DisplayMetrics metrics = getDisplayMetrics(activity);
 
         activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
@@ -32,6 +36,17 @@ public class DeviceInfos {
 
 
         return screenInfos;
+    }
+
+    private static DisplayMetrics getDisplayMetrics(Activity activity) {
+        DisplayMetrics metric = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) { //Android 4.2,4.2.2 api level 17
+            WindowManager windowMgr = (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
+            windowMgr.getDefaultDisplay().getRealMetrics(metric);
+        } else {
+            activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        }
+        return metric;
     }
 
 }
